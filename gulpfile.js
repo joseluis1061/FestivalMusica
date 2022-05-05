13df9427a6f43d8d405efae115307cf7bbc94ead
jose <joseluis836@hotmail.com>
2022-05-05 10:16:00 -0500

    Se agrego la dependencia gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,15 @@
 const{src, dest, watch} = require('gulp');
 //Requiere el puente entre gulp-sass y la libreria de sass
 const sass = require('gulp-sass')(require('sass'));
+//Instanciar plumber evita el cierre de ejecución por errores
+const plumber = require('gulp-plumber');
 
 //Función para compilar scss a salida css
 function css(done){
     //Identificar la cartpeta de orignen de estilos
     src("src/scss/**/*.scss")
+    // Evitar el cierre por errores
+        pipe(plumber())
     //Compilarla.. pipe para una tarea secuencial
         .pipe(sass())
     //Almacenar la salida en estilos con extensión css 
